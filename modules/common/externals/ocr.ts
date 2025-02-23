83a0bb9767a05117fc3afad0584f34cab0ec522e
jissp <jissp@naver.com>
2025-02-23 22:33:39 +0900

    feature: 바람의나라 Support 스크립트
    - OCR External 모듈 추가

---
+++ modules/common/externals/ocr.ts
@@ -0,0 +1,23 @@
+import * as path from 'node:path';
+
+const { execFile } = require('child_process');
+
+export async function ocr(imagePath: string): Promise<string> {
+    const swiftExecutable = path.resolve('./modules/common/externals/bin/ocr');
+
+    return new Promise((resolve, reject) => {
+        execFile(swiftExecutable, [imagePath], (error: Error, stdout: string, stderr: string) => {
+            if (error) {
+                reject(error.message);
+                console.error(`오류 발생: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                reject(stderr);
+                return;
+            }
+
+            resolve(stdout);
+        });
+    });
+}
