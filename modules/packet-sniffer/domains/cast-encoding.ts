8eccfe0f02ac1e9b543b97ede3ac5210c3557de6
jissp <jissp@naver.com>
2025-03-07 19:23:17 +0900

    feature: 바람의니라 스크립트
    - castEncoding toString Encode 오류 수정

--- modules/packet-sniffer/domains/cast-encoding.ts
+++ modules/packet-sniffer/domains/cast-encoding.ts
@@ -1,3 +1,3 @@
 export function castEncoding(value: string, from: BufferEncoding, to: BufferEncoding) {
-    return Buffer.from(value, from).toString('hex');
+    return Buffer.from(value, from).toString(to);
 }
